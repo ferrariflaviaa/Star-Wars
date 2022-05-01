4413df5e04674ff45d9024ea1ba084b932c309cf
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-05-01 14:21:19 -0300

    usando styled-componentes

--- src/Components/CharacterListPage/CharacterListPage.js
+++ src/Components/CharacterListPage/CharacterListPage.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import api from "../../Services/api";
+import { Persona } from "./style";
 
 export default function CharacterListPage() {
   const [CharacterList, setCharacterList] = useState([]);
 
-    //FUNÇÃO PARA REQUISIÇÃO: ELA LOGA OS RESULTADOS E SALVA ELE DENTRO DE UM ESTADO TIPO DE VETOR.
-    async function getCharacterList() {
-      await api
-        .get('/people')
-        .then((data) => {
-          console.log(data.data.results)
-          setCharacterList(data.data.results);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }  
-
+  //FUNÇÃO PARA REQUISIÇÃO: ELA LOGA OS RESULTADOS E SALVA ELE DENTRO DE UM ESTADO TIPO DE VETOR.
+  async function getCharacterList() {
+    await api
+      .get("/people")
+      .then((data) => {
+        console.log(data.data.results);
+        setCharacterList(data.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   //AO INICIAR ESSE ARQUIVO ELE CHAMA FUNÇÃO getCharacterList():
   useEffect(() => {
     getCharacterList();
   }, []);
 
-  return ( 
-      <div>
-        {CharacterList.map(item => {
-           return <h1 key={item.name+item.birth_year}>{item.name}</h1>
-        })}
-      </div>
+  return (
+    <div>
+      {CharacterList.map((item) => {
+        return <Persona key={item.name + item.birth_year}>{item.name}</Persona>;
+      })}
+    </div>
   );
 }
---
+++ src/Components/CharacterListPage/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Persona = styled.h1`
+    border: 1px solid #FF0000;
+    margin: 0px;
+    margin-bottom: 4px;
+`
