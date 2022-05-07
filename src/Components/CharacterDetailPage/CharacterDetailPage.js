d89ffe22d8b49748e9f6968cfc69f30a9df88eb6
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-05-07 10:15:51 -0300

    renderizando as informações do personagem

--- src/Components/CharacterDetailPage/CharacterDetailPage.js
+++ src/Components/CharacterDetailPage/CharacterDetailPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import api from "../../Services/api";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 export default function CharacterDetailPage() {
   const [CharacterDetails, setCharacterDetails] = useState();  
@@ -11,8 +13,8 @@ export default function CharacterDetailPage() {
     await api
       .get(`/people/${peopleId}`)
       .then((data) => {
-        console.log(data.data);
-        setCharacterDetails(data.data.results);
+        // console.log(data.data);
+        setCharacterDetails(data.data);
       })
       .catch((error) => {
         console.log(error);
@@ -23,9 +25,32 @@ export default function CharacterDetailPage() {
     getCharacterDetails();
   }, []);
 
+  useEffect(() => {
+    console.log(CharacterDetails);
+  }, [CharacterDetails]);
+
   return (
     <div>
-      <h1>Detalhe</h1>
+      <h1>Detalhes</h1>
+      <Link to="/" >
+        <button type="submit" name="myButton" >Voltar</button>
+      </Link>
+
+      {CharacterDetails && (
+        <div>
+          <h1>Nome: {CharacterDetails.name}</h1>
+          <h1>Ano de Nascimento: {CharacterDetails.birth_year}</h1>
+          <h1>Criado: {CharacterDetails.created}</h1>
+          <h1>Editado: {CharacterDetails.edited}</h1>
+          <h1>Cor dos olhos: {CharacterDetails.eye_color}</h1>
+          <h1>Altura: {CharacterDetails.height}</h1>
+          <h1>Cor dos olhos: {CharacterDetails.eye_color}</h1>
+          <h1>Gênero: {CharacterDetails.gender === 'n/a' ? 'Não informado' : CharacterDetails.gender} </h1>
+          <h1>Cor do cabelp: {CharacterDetails.hair_color === 'n/a' ? 'Não informado' : CharacterDetails.hair_color} </h1>
+        </div>
+      )}
+
+      
     </div>
   );
 }
